3594f81404297059a6e491ed27fc2f030a475775
roelderickx <roel.derickx@gmail.com>
2018-12-30 22:04:56 +0100

    Added rendering of waypoints

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # hikingmap-web
 
-This repository is still under development. Please come back later.
+This is the PHP version of hikingmap. It aims at providing a web interface to hikingmap, eliminating the need to setup a database and cartoCSS stylesheet.
+
+## Limitations
+
+This repository is still in a testing phase, though it may already be useful to you. The python code is almost fully converted to PHP and the rendering is replaced by OSM map tiles served on openstreetmap.org. However, due to [the usage policy](https://operations.osmfoundation.org/policies/tiles/) only a small preview of each page is rendered.
+Also, the scaling is not yet correct. The math works but it is unclear how the boundaries of a map can be set with the OpenLayers library.
 
--- index.php
+++ index.php
@@ -24,55 +24,64 @@
     <title>Hikingmap web</title>
 </head>
 <body>
+    <div id="parameterform">
     <form action="hikingmap.php" method="post" enctype="multipart/form-data">
-        <table>
-        <tr>
-        <td><label for="gpxfile">GPX Track</label></td>
-        <td colspan="2"><input type="file" id="gpxfile" name="gpxfile"/></td>
-        </tr>
-        <tr>
-        <td><label for="scale">Scale</label></td>
-        <td colspan="2">
-            1:<input type="text" id="scale" name="scale" value="50000" size="7"/>
-        </td>
-        </tr>
-        <tr>
-        <td><label for="width">Paper width</label></td>
-        <td colspan="2">
-            <input type="text" id="width" name="width" value="200" size="4"/> mm
-        </td>
-        </tr>
-        <tr>
-        <td><label for="height">Paper height</label></td>
-        <td colspan="2">
-            <input type="text" id="height" name="height" value="287" size="4"/> mm
-        </td>
-        </tr>
-        <tr>
-        <td><label for="overlap">Page overlap</label></td>
-        <td colspan="2">
-            <input type="text" id="overlap" name="overlap" value="10" size="4"/> mm
-        </td>
-        </tr>
-        <tr>
-        <td><label for="waypoints">Add waypoints</label></td>
-        <td><input type="checkbox" id="waypoints" name="waypoints" value="true" checked/></td>
-        <td>
-            every
-            <input type="text" name="waypointsdist" value="1" size="2"/>
-            <select id="lengthunit" name="lengthunit">
-                <option value="km" selected>km</option>
-                <option value="mi">mi</option>
-            </select>
-        </td>
-        </tr>
-        <tr>
-        <td colspan="3"><input type="submit" value="Submit"/></td>
-        <!--td colspan="2"><input type="reset" value="Reset" width="100"/></td-->
-        </tr>
-        </table>
+        <fieldset>
+            <legend>Parameters</legend>
+            <table>
+            <tr>
+            <td><label for="gpxfile">GPX Track</label></td>
+            <td colspan="2"><input type="file" id="gpxfile" name="gpxfile"/></td>
+            </tr>
+            <tr>
+            <td><label for="scale">Scale</label></td>
+            <td colspan="2">
+                1:<input type="text" id="scale" name="scale" value="50000" size="7"/>
+            </td>
+            </tr>
+            <tr>
+            <td><label for="width">Paper width</label></td>
+            <td colspan="2">
+                <input type="text" id="width" name="width" value="200" size="4"/> mm
+            </td>
+            </tr>
+            <tr>
+            <td><label for="height">Paper height</label></td>
+            <td colspan="2">
+                <input type="text" id="height" name="height" value="287" size="4"/> mm
+            </td>
+            </tr>
+            <tr>
+            <td><label for="overlap">Page overlap</label></td>
+            <td colspan="2">
+                <input type="text" id="overlap" name="overlap" value="10" size="4"/> mm
+            </td>
+            </tr>
+            <tr>
+            <td><label for="waypoints">Add waypoints</label></td>
+            <td>
+                <input type="checkbox" id="waypoints" name="waypoints" value="true" checked/>
+            </td>
+            <td>
+                <label for="waypointsdist">every</label>
+                <input type="text" id="waypointsdist" name="waypointsdist" value="1" size="2"/>
+                <select id="lengthunit" name="lengthunit">
+                    <option value="km" selected>km</option>
+                    <option value="mi">mi</option>
+                </select>
+            </td>
+            </tr>
+            <tr>
+            <td colspan="3"><input type="submit" value="Submit"/></td>
+            <!--td colspan="2"><input type="reset" value="Reset" width="100"/></td-->
+            </tr>
+            </table>
+        </fieldset>
     </form>
+    </div>
+    <div id="poweredby">
     Powered by <a href="https://github.com/roelderickx/hikingmap-web">hikingmap-web</a>.
+    </div>
 </body>
 </html>
 
